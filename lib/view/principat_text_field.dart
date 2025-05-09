f60e93e19a73bb8c8226089faaf81f13e014cc80
PACOdergod <paco.33353@gmail.com>
2025-05-09 10:22:44 -0600

    Ahora la app validará cada cambio en el TextField y mostrará un error en caso de que el valor introducido por el usuario no sea válido. En caso de que el input esté fuera del rango permitido por la dificultad, no permitiraáingresar el valor

--- lib/view/principat_text_field.dart
+++ lib/view/principat_text_field.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
+import '../difficult_cubit/difficult_cubit.dart';
 import '../game_cubit/game_cubit.dart';
 
 class PrincipatTextField extends StatefulWidget {
@@ -13,52 +15,97 @@ class PrincipatTextField extends StatefulWidget {
 
 class _PrincipatTextFieldState extends State<PrincipatTextField> {
   late final TextEditingController numberController;
+  late final ValueNotifier<String?> errorMessageNotifier;
 
   @override
   void initState() {
     numberController = TextEditingController();
+    errorMessageNotifier = ValueNotifier(null);
     super.initState();
   }
 
   @override
   void dispose() {
     numberController.dispose();
+    errorMessageNotifier.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: numberController,
-      decoration: InputDecoration(
-        contentPadding: const EdgeInsets.all(16),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.grey),
+    return HookBuilder(builder: (context) {
+      useListenable(errorMessageNotifier);
+
+      return TextField(
+        controller: numberController,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(16),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.grey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.blue, width: 2),
+          ),
+          labelText: 'Number',
+          labelStyle:
+              const TextStyle(color: Colors.grey, fontWeight: FontWeight.w500),
+          floatingLabelStyle: const TextStyle(color: Colors.blue),
+          hintText: '####',
+          hintStyle: const TextStyle(color: Colors.grey),
+          errorText: errorMessageNotifier.value,
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.red, width: 2),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: const BorderSide(color: Colors.red, width: 2),
+          ),
         ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: const BorderSide(color: Colors.blue, width: 2),
-        ),
-        labelText: 'Number',
-        labelStyle:
-            const TextStyle(color: Colors.grey, fontWeight: FontWeight.w500),
-        floatingLabelStyle: const TextStyle(color: Colors.blue),
-        hintText: '####',
-        hintStyle: const TextStyle(color: Colors.grey),
-        errorText: 'Error',
-      ),
-      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-      keyboardType: TextInputType.number,
-      onSubmitted: (value) {
-        _onSubmittValue(context, value);
-      },
-    );
+        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+        keyboardType: TextInputType.number,
+        onSubmitted: (value) {
+          onSubmittValue(context, value);
+        },
+        onChanged: (value) {
+          validateUserInput(value);
+        },
+      );
+    });
   }
 
-  void _onSubmittValue(BuildContext context, String value) {
-    final number = int.tryParse(value) ?? 1;
+  void validateUserInput(String input) {
+    if (input.isEmpty) {
+      errorMessageNotifier.value = null;
+      return;
+    }
 
+    final number = int.tryParse(input);
+
+    if (number == null) {
+      errorMessageNotifier.value = 'No valido';
+      return;
+    }
+
+    final DifficultCubit difficultCubit = context.read();
+    final difficultState = difficultCubit.state;
+
+    if (number < difficultState.minimum) {
+      errorMessageNotifier.value = 'El número está fuera del rango permitido';
+    } else if (number > difficultState.maximum) {
+      errorMessageNotifier.value = 'El número está fuera del rango permitido';
+    } else {
+      errorMessageNotifier.value = null;
+    }
+  }
+
+  void onSubmittValue(BuildContext context, String value) {
+    if (errorMessageNotifier.value != null) return;
+    if (value.isEmpty) return;
+
+    final number = int.tryParse(value) ?? 1;
     final gameCubit = context.read<GameCubit>();
     gameCubit.onSubmittValue(number);
     numberController.clear();
